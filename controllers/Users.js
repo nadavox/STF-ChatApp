eaf4b56ab2ad6a3becfc8c1182468b34c88f02b5
Daniel Cohen Shor <danielcohenshor@gmail.com>
2023-05-24 10:13:00 +0300

    update

--- controllers/Users.js
+++ controllers/Users.js
@@ -8,55 +8,62 @@ const createNewUser = async (req, res) => {
     const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{5,}$/;
     const usernameRegex = /[a-zA-Z]/;
     const invalidFields = [];
+    var invalid = 0;
 
     // check if Username input is not empty
     if ((!username)) {
         invalidFields.push('username');
-        //throw new Error('required field - must contain at least one letter');
     }
     // check if the username meets the requirements of the regex
     else if (!usernameRegex.test(username)) {
         invalidFields.push('username');
-        //throw new Error('must contain at least one letter');
     }
     // check if the username already exist
-    try {
-        userService.validateUsername(username);
-    }
-    catch(error) {
-        invalidFields.push('username');
-    }
+    // if (await userService.validateUsername(username) < 0) {
+    //     return res.status(409).json({
+    //         type: "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+    //         title: "Conflict",
+    //         status: 409,
+    //         traceId: "0HMQS45UOJOV4:00000002"
+    //     });
+    // }
+
+    invalid = await userService.validateUsername(username);
 
     // check if password input is not empty and meets the requirements of the regex
     if (!password || !passwordRegex.test(password)) {
         invalidFields.push('password');
-        //throw new Error('invalid password');
     }
-
-    // // check if password input is not empty and meets the requirements of the regex
-    // if (!ConfirmPassword || !passwordRegex.test(ConfirmPassword) || ConfirmPassword !== Password) {
-    //     invalidFields.push('Password Verification');
-    // }
 
     // check if displayName input is not empty and meets the requirements of the regex
     if (!displayName || !usernameRegex.test(displayName)) {
         invalidFields.push('displayName');
-        //throw new Error('invalid displayName');
     }
 
     // check if profilePic input is not empty
     if (!profilePic) {
         invalidFields.push('profilePic');
-        //throw new Error('invalid profilePic');
     }
 
-    console.log(invalidFields);
-
-    if (invalidFields.length === 0) {
-        console.log("in");
+    if (invalidFields.length === 0 && invalid > 0) {
         res.json(await userService.createNewUser(username, password, displayName, profilePic));
     } else if (invalidFields.length > 0) {
-        return res.status(400).json({ errors: 'Invalid fields', invalidFields });
+        return res.status(400).json({
+            type: "https://tools.ietf.org/html/rfc7231#section-6.5.1",
+            title: "One or more validation errors occurred.",
+            status: 400,
+            traceId: "0HMQS45UOJOV2:00000006",
+            errors: invalidFields
+        });
+    }
+
+    if (invalid < 0) {
+        return res.status(409).json({
+            type: "https://tools.ietf.org/html/rfc7231#section-6.5.8",
+            title: "Conflict",
+            status: 409,
+            traceId: "0HMQS45UOJOV4:00000009"
+        });
     }
 };
 
