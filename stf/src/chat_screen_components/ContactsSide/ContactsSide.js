b74767a32c90e439fb85082aaa9b6945a376bac6
Daniel Cohen Shor <danielcohenshor@gmail.com>
2023-05-29 15:16:04 +0300

    make the search work

--- stf/src/chat_screen_components/ContactsSide/ContactsSide.js
+++ stf/src/chat_screen_components/ContactsSide/ContactsSide.js
@@ -4,14 +4,16 @@ import UserRow from '../UserRow/UserRow';
 import React, { useState, useEffect, useContext } from 'react';
 import { CurrentUserContext } from '../../components/CurrentUser/CurrentUser';
 import SearchContact from '../SearchContact/SearchContact';
-import MessagesScreen from '../MessagesScreen/MessagesScreen';
 import getchats from '../../auth/GetChats';
 
 function ContactsSide(props) {
 
     const { currentUser } = useContext(CurrentUserContext);
     //inisde of the list of contacts is all the contacts of the user.
-    const [ListOfContacts, setListOfContacts] = useState([])
+    const [listOfContacts, setListOfContacts] = useState([]);
+    const [finalSearchValue, setFinalSearchValue] = useState("");
+    const [filteredContacts, setFilteredContacts] = useState([]);
+    const [textInSearch, setTextInSearch] = useState(false);
 
     async function getcontacts() {
         const l = await getchats(currentUser)
@@ -21,7 +23,7 @@ function ContactsSide(props) {
 
     // in contact id i have the key of the chat
     const handleClickingOnContact = async (contactId) => {
-        const currSelectedContact = ListOfContacts.find((contact) => contact.id === contactId);
+        const currSelectedContact = listOfContacts.find((contact) => contact.id === contactId);
         props.setDisplayContactRow({
             picture: currSelectedContact.user.profilePic,
             displayName: currSelectedContact.user.displayName,
@@ -29,10 +31,10 @@ function ContactsSide(props) {
         });
         // update the current contact that we click
         props.setClickContact(contactId)
-        // if(textInSearch) {
-        //     setTextInSearch(false);
-        //     setFinalSearchValue("");
-        // }
+        if(textInSearch) {
+            setTextInSearch(false);
+            setFinalSearchValue("");
+        }
     };
 
     // init the contacts list
@@ -51,7 +53,7 @@ function ContactsSide(props) {
             date.getDate() === currentDate.getDate()
         );
 
-        if(isCurrentDate) {
+        if (isCurrentDate) {
             const hours = date.getHours();
             const minutes = date.getMinutes();
             return `${hours}:${minutes}`;
@@ -60,7 +62,7 @@ function ContactsSide(props) {
             const day = date.getDate();
             const month = date.getMonth() + 1; // Add 1 because getMonth() returns zero-based month
             const year = date.getFullYear();
-            
+
             return `${day}.${month}.${year}`;
         }
     }
@@ -80,55 +82,52 @@ function ContactsSide(props) {
         }
     }, [props.testCounterOfMessages])
 
+    function handleChange(event) {
+        const filtered = listOfContacts.filter(
+            (contact) => { return contact.user.displayName.includes(event); });
+        setFilteredContacts(filtered);
+        if (event !== "") {
+            setTextInSearch(true);
+        } else {
+            setTextInSearch(false);
+        }
+    }
+
     return (
         <>
             <div className="col-1 p-0 d-flex flex-column flex-grow-1 position-relative leftSide">
-                {/* need to add onclick function to the userRow */}
                 <UserRow picture={currentUser.photoUrl} firstName={currentUser.displayName}
                     setPressed={props.setPressedOnAddContact} setaddContact={props.setaddContact} />
 
-                {/* <SearchContact onKeyDown={handleKeyDown} onChangeInput={handleChange} setInputValue={setFinalSearchValue} value={finalSearchValue} /> */}
+                <SearchContact onChangeInput={handleChange} setInputValue={setFinalSearchValue} value={finalSearchValue} />
 
                 <ul id="contactsList">
-                    {ListOfContacts.map((contact) => (
-                        <Contact
-                            key={contact.id}
-                            photoUrl={contact.user.profilePic}
-                            contactDispalyName={contact.user.displayName}
-                            lastMessageTime={contact.lastMessage && contact.lastMessage.content ? generateTime(contact.lastMessage.created) : ""}
-                            lastMessage={contact.lastMessage && contact.lastMessage.content ? contact.lastMessage.content : "no-meesage"}
-                            notification=""
-                            className={props.currentContactClicked === contact.id ? 'selected' : ''}
-                            onClick={() => handleClickingOnContact(contact.id)}
-                        />
-                    ))}
-
-                    {/* {filteredContacts.length >= 0 && textInSearch ?
+                    {filteredContacts.length >= 0 && textInSearch ?
                         filteredContacts.map(contact => (
                             <Contact
-                                key={contact.username}
-                                photoUrl={contact.photoUrl}
-                                contactDispalyName={contact.contactDispalyName}
-                                lastMessageTime={contact.lastMessageTime}
-                                lastMessage={contact.lastMessage}
-                                notification={contact.notification}
-                                className={props.currentContactClicked === contact.username ? 'selected' : ''}
-                                onClick={() => handleClickingOnContact(contact.username)}
-                            />
-                        )) :
-                        ListOfContacts.map(contact => (
-                            <Contact
-                                key={contact.user.username}
+                                key={contact.id}
                                 photoUrl={contact.user.profilePic}
                                 contactDispalyName={contact.user.displayName}
-                                lastMessageTime={contact.lastMessageTime}
-                                lastMessage={contact.lastMessage}
-                                notification={contact.notification}
-                                className={props.currentContactClicked === contact.username ? 'selected' : ''}
-                                onClick={() => handleClickingOnContact(contact.username)}
+                                lastMessageTime={contact.lastMessage && contact.lastMessage.content ? generateTime(contact.lastMessage.created) : ""}
+                                lastMessage={contact.lastMessage && contact.lastMessage.content ? contact.lastMessage.content : "no-meesage"}
+                                notification=""
+                                className={props.currentContactClicked === contact.id ? 'selected' : ''}
+                                onClick={() => handleClickingOnContact(contact.id)}
+                            />
+                        )) :
+                        listOfContacts.map(contact => (
+                            <Contact
+                                key={contact.id}
+                                photoUrl={contact.user.profilePic}
+                                contactDispalyName={contact.user.displayName}
+                                lastMessageTime={contact.lastMessage && contact.lastMessage.content ? generateTime(contact.lastMessage.created) : ""}
+                                lastMessage={contact.lastMessage && contact.lastMessage.content ? contact.lastMessage.content : "no-meesage"}
+                                notification=""
+                                className={props.currentContactClicked === contact.id ? 'selected' : ''}
+                                onClick={() => handleClickingOnContact(contact.id)}
                             />
                         ))
-                    } */}
+                    }
                 </ul>
 
             </div>
